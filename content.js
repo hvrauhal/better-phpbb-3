62fac2874651e198b5b084b6208c3291b3da312b
Heikki Rauhala <hvrauhal@gmail.com>
2015-02-02 12:22:43 +0200

    Only render the open all in tabs link if there are topics to open

--- content.js
+++ content.js
@@ -5,13 +5,18 @@
     })
   }
 
-  function open_all_in_tabs(e) {
-    e.preventDefault();
-    chrome.extension.sendRequest(find_links());
+  function open_all_in_tabs(links) {
+    chrome.extension.sendRequest(links);
   }
 
-  $(".action-bar").append('<a class="open_all_in_tabs" href="#">Open all topics in tabs</a>');
-  $(".open_all_in_tabs").click(open_all_in_tabs);
+  var links = find_links();
+  if (links.length > 0) {
+    $(".action-bar").append('<a class="open_all_in_tabs" href="#">Open all topics in tabs</a>');
+    $(".open_all_in_tabs").click(function (e) {
+      e.preventDefault();
+      open_all_in_tabs(links);
+    });
+  }
   $("#page-footer").prepend($('#page-header .navbar').clone().find('#nav-breadcrumbs').remove().end())
 })();
 
